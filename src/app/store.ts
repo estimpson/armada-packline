c2c03b1e39e8de09f90bf99ad1be16349a341d31
Eric Stimpson <estimpson@fore-thought.com>
2021-08-28 09:09:03 -0400

    implement redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,22 @@
-import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import identityReducer from '../features/identity/identitySlice';
+import {
+    Action,
+    combineReducers,
+    configureStore,
+    ThunkAction,
+} from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+
+const reducers = combineReducers({ identity: identityReducer });
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-    reducer: {
-        identity: identityReducer,
-    },
+    reducer: persistedReducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,21 @@ import reportWebVitals from './reportWebVitals';
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Set up redux store
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import persistStore from 'redux-persist/es/persistStore';
+import { PersistGate } from 'redux-persist/integration/react';
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root'),
