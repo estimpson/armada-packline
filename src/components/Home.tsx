68de970126f131deb2d259528232c123fda680e7
Eric Stimpson <estimpson@fore-thought.com>
2021-08-31 08:14:05 -0400

    user instructions

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,9 @@
+import {
+    faPencilAlt,
+    faSdCard,
+    faUndoAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
@@ -20,10 +26,75 @@ export default function Home() {
             <Container>
                 {identity?.userName ? (
                     <>
-                        <h1>Welcome {identity.userName}</h1>
-                        <Button onClick={() => dispatch(logout())}>
-                            Logout
-                        </Button>
+                        <Container className="d-flex justify-content-center px-0 mb-5">
+                            <p className="display-6">
+                                Welcome, {identity.userName}
+                            </p>
+                            <Button
+                                className="mx-5 my-auto"
+                                onClick={() => dispatch(logout())}
+                            >
+                                Logout
+                            </Button>
+                        </Container>
+                        <p className="bd-lead">
+                            The Aztec Supplier Portal is here to assist you in
+                            meeting Aztec's labelling requirements.
+                        </p>
+                        <p className="text-justify">
+                            When you ship product to any of Aztec's locations,
+                            please be sure to label that product in accordance
+                            with Aztec's labelling requirements. This portal can
+                            be used to print labels on virtually any thermal
+                            label printer, such as Zebra, Sato, or Intermec.
+                        </p>
+                        <p className="text-justify">
+                            To get started using this portal, visit the{' '}
+                            <span className="text-light bg-dark px-1">
+                                Printers
+                            </span>{' '}
+                            screen and select the locally attached printer that
+                            you want to use when printing Aztec's labels. You'll
+                            need to do this once on every PC you use to print
+                            labels using this portal. You don't have to change
+                            your Window's default printer, and in fact you
+                            probably don't want to unless you always print to
+                            the thermal label printer from this computer. While
+                            you're there, you can print a test page to make sure
+                            your printer is working properly.
+                        </p>
+                        <p className="text-justify">
+                            Once you're able to print successfully, you can
+                            generate new labels from the{' '}
+                            <span className="text-light bg-dark px-1">
+                                Generate Inventory
+                            </span>{' '}
+                            screen. Fill in the form at the top with all of the
+                            required information, and click the button to create
+                            a batch of inventory. If you need to edit the
+                            quantity or lot number of the individual serials in
+                            the batch, you can use the{' '}
+                            <FontAwesomeIcon icon={faPencilAlt} /> icon to edit
+                            the batch one serial at a time. Use the{' '}
+                            <FontAwesomeIcon icon={faSdCard} /> icon to save
+                            your edits or the{' '}
+                            <FontAwesomeIcon icon={faUndoAlt} /> icon to cancel
+                            your changes. When your ready to print labels, use
+                            the top checkbox to select all of the serials in the
+                            batch or the individual checkboxes on each serial to
+                            print just a subset.
+                        </p>
+                        <p className="text-justify">
+                            If you need to reprint any labels from a previous
+                            batch, the{' '}
+                            <span className="text-light bg-dark px-1">
+                                Reprint Labels
+                            </span>{' '}
+                            screen is here to help. Simply select the lot number
+                            corresponding to a previously created batch to edit
+                            (optionally) and reprint any or all labels in that
+                            batch.
+                        </p>
                     </>
                 ) : (
                     <>
