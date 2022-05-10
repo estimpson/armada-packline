563c345148d17120ade938961e56391b40950397
Eric Stimpson <estimpson@fore-thought.com>
2022-05-10 16:35:46 -0400

    provide ping

---
+++ dotnet/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("Ping")]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
