58ff44393087f5cf9417f320387acb9ff6dc0a1b
Eric Stimpson <estimpson@fore-thought.com>
2022-05-14 09:36:18 -0400

    bartender integrated printing for packing job objects (and combined objects)

--- dotnet/Controllers/PrintController.cs
+++ dotnet/Controllers/PrintController.cs
@@ -25,6 +25,54 @@ namespace api.Controllers
             _logger = logger;
             _fxContext = fxContext;
         }
+
+        [HttpPost("printPackingJobBT")]
+        public IEnumerable<PackingJobObject> PrintPackingJobBT([FromQuery] string user, [FromQuery] string packingJobNumber)
+        {
+            var openedPackingJobObjects = OpenPackingJobObjectsForPrint(user, packingJobNumber);
+
+            string bartenderPath;
+            if (SysFile.Exists(@"c:\Program Files (x86)\Seagull\BarTender Suite\bartend.exe"))
+            {
+                bartenderPath = @"""c:\Program Files (x86)\Seagull\BarTender Suite\bartend.exe""";
+            }
+            else if (SysFile.Exists(@"c:\Program Files (x86)\Seagull\BarTender Suite 2021\bartend.exe"))
+            {
+                bartenderPath = @"""c:\Program Files (x86)\Seagull\BarTender Suite 2021\bartend.exe""";
+            }
+            else if (SysFile.Exists(@"c:\Program Files\Seagull\BarTender Suite\bartend.exe"))
+            {
+                bartenderPath = @"""c:\Program Files\Seagull\BarTender Suite\bartend.exe""";
+            }
+            else if (SysFile.Exists(@"c:\Program Files\Seagull\BarTender Suite 2021\bartend.exe"))
+            {
+                bartenderPath = @"""c:\Program Files\Seagull\BarTender Suite 2021\bartend.exe""";
+            }
+            else
+            {
+                throw new BadHttpRequestException("BarTender not found");
+            }
+
+            foreach (var openedPackingJobObject in openedPackingJobObjects)
+            {
+                var bartenderArgs = @$"/F=""{openedPackingJobObject.LabelPath}"" /?Serial={openedPackingJobObject.Serial} /C={openedPackingJobObject.Copies} /P /X";
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = bartenderPath,
+                        Arguments = bartenderArgs
+                    }
+                };
+                process.Start();
+                process.WaitForExit();
+            }
+
+            var closedPackingJobObjects = ClosePackingJobPreObjectsAfterPrint(user, packingJobNumber, true);
+
+            return closedPackingJobObjects;
+        }
+
         [HttpPost("printSerialBT")]
         public PackingJobObject PrintSerialBT([FromQuery] string user, [FromQuery] long serial, [FromQuery] string labelPath, int copies)
         {
@@ -99,7 +147,39 @@ execute FXPL.usp_CRUD_ClosePreObjectAfterPrint
                 new StringReader($"<Result>{result.Result}</Result>"))).ToArray()[0];
 
             return packingJobObject;
-
         }
-    }
+
+        private IEnumerable<PackingJobObjectForPrint> OpenPackingJobObjectsForPrint(string user, string packingJobNumber)
+        {
+            var result = _fxContext.XmlResults.FromSqlInterpolated($@"
+execute FXPL.usp_CRUD_OpenPackingJobPreObjectsForPrint
+    @User = {user}
+,   @PackingJobNumber = {packingJobNumber}
+").ToArray()[0];
+
+
+            var deserializer = new XmlSerializer(typeof(List<PackingJobObjectForPrint>), new XmlRootAttribute("Result"));
+            var packingJobObjects = (List<PackingJobObjectForPrint>)deserializer.Deserialize(
+                new StringReader($"<Result>{result.Result}</Result>"));
+
+            return packingJobObjects;
+        }
+
+        private IEnumerable<PackingJobObject> ClosePackingJobPreObjectsAfterPrint(string user, string packingJobNumber, bool printed)
+        {
+            var result = _fxContext.XmlResults.FromSqlInterpolated(
+            $@"
+execute FXPL.usp_CRUD_ClosePackingJobPreObjectsAfterPrint
+    @User = {user}
+,   @PackingJobNumber = {packingJobNumber}
+,   @Printed = {(printed ? 1 : 0)}
+").ToArray()[0];
+
+            var deserializer = new XmlSerializer(typeof(List<PackingJobObject>), new XmlRootAttribute("Result"));
+            var packingJobObjects = ((List<PackingJobObject>)deserializer.Deserialize(
+                new StringReader($"<Result>{result.Result}</Result>")));
+
+            return packingJobObjects;
+        }
+   }
 }
---
+++ dotnet/Models/PackingJobObjectForPrint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace api.Models
+{
+    [XmlRoot]
+    public class PackingJobObjectForPrint
+    {
+        [XmlAttribute] public string PackingJobNumber { get; set; }
+
+        [XmlAttribute] public long Serial { get; set; }
+
+        [XmlAttribute] public decimal Quantity { get; set; }
+
+        [XmlAttribute] public bool Printed { get; set; }
+
+        [XmlAttribute] public string LabelPath { get; set; }
+        
+        [XmlAttribute] public int Copies { get; set; }
+
+        [XmlAttribute] public int RowID { get; set; }
+    }
+}
